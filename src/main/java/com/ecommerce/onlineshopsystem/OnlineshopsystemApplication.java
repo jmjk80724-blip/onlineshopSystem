26c1ea02d3af6494829a848b5e3b1d39a1de94ec
jmjk80724-blip <jmjk80724@gmail.com>
2026-08-11 17:28:01 +0700

    Initial commit: Spring Boot with PostgreSQL

---
+++ src/main/java/com/ecommerce/onlineshopsystem/OnlineshopsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlineshopsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshopsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineshopsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/onlineshopsystem/OnlineshopsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.onlineshopsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
