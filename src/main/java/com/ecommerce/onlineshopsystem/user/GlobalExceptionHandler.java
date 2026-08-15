94e023d770e25bd305dfe3184722ede0610ed404
jmjk80724-blip <jmjk80724@gmail.com>
2026-08-15 18:25:21 +0700

    Complet User Feature : Entity, Repositoty, Sevice, Controller, Security config, Dto,Exception Handle

--- src/main/java/com/ecommerce/onlineshopsystem/user/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/onlineshopsystem/user/GlobalExceptionHandler.java
@@ -9,14 +9,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.sun.tools.javac.jvm.ByteCodes.error;
+
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Return 404
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<Map<String,String>> handleUserNotFoundException(UserNotFoundException ex) {
-       Map<String,String> map = new HashMap<>();
+       Map<String,String> error = new HashMap<>();
        error.put("error", ex.getMessage());
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
-}
+       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+    //Return 400
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneral(Exception ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error","Something went wrong" + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+    }
 }
